bf3388894a2a9f9c84c1d2adc7caa97349f3a096
NoldemPasso <vladocouille@gmail.com>
2025-10-15 18:40:23 +0200

    test

---
+++ src/main/java/net/noldem/difficultrealismmod/item/ModItems.java
@@ -0,0 +1,25 @@
+package net.noldem.difficultrealismmod.item;
+
+import net.minecraft.world.item.Item;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+import net.noldem.difficultrealismmod.DifficultRealismMod;
+
+public class ModItems {
+    public static final DeferredRegister<Item> ITEMS =
+            DeferredRegister.create(ForgeRegistries.ITEMS, DifficultRealismMod.MOD_ID);
+
+    public static final RegistryObject<Item> ALEXANDRITE = ITEMS.register("alexandrite",
+            () -> new Item(new Item.Properties()));
+
+    public static final RegistryObject<Item> RAW_ALEXANDRITE = ITEMS.register("raw_alexandrite",
+            () -> new Item(new Item.Properties()));
+
+
+
+    public static void register(IEventBus eventBus){
+        ITEMS.register(eventBus);
+    }
+}
